04bba4f99a401d7896c0acd9ff025731b2924158
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-17 15:51:07 +0200

    Added Javadoc comments.

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
-import java.util.Scanner;
+/**
+ * Use this file as a general sandbox.
+ * @author David Browning (davbro-4)
+ */
+
 public class Main {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
