8451c1050ecf139a04b3ccd1ff6933c9eaa2d8a5
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-17 12:23:21 +0200

    Testing opening/closing two Scanner objects (does not work).

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = userInput.nextLine();
+        userInput.close();
+
+        Scanner userInput2 = new Scanner(System.in);
+        System.out.print("Enter another string: ");
+        String str2 = userInput2.nextLine();
+        userInput2.close();
+    }
+}
