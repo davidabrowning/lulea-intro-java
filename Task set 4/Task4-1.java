2b6e20bd0199d4f8d77bda73027d78fc53e65b27
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-23 19:48:48 +0200

    Added program instructions and description.

---
+++ Task set 4/Task4-1.java
@@ -0,0 +1,16 @@
+// Imports
+
+/**
+ * Instructions:
+ * In Sweden, the driving license age is currently 18 years. 
+ * Write a program that reads in a particular person's age 
+ * and then, based on the person's age, prints whether the 
+ * person is eligible for a driving license.
+ * 
+ * Program description:
+ * 1. Ask user for age
+ * 2. Check if the user can drive
+ * 3. Print the result
+ * 
+ * @author David Browning (davbro-4)
+ */
