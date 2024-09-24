7e25df057085f0ab544062c223cd9ce9b54a16ad
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-24 13:03:10 +0200

    Create .java and set up scaffolding.

---
+++ TaskSet04/Task0404.java
@@ -0,0 +1,22 @@
+package TaskSet04;
+
+// Imports
+
+/**
+ * TASK:
+ * 
+ * PROGRAM DESCRIPTION:
+ * 
+ * @author David Browning (davbro-4)
+ */
+public class Task0404 {
+
+    // Declare constants
+
+    public static void main(String[] args) {
+
+        // Declare variables
+
+    }
+    
+}
