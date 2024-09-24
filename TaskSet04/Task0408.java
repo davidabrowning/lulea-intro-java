15803e34d1e0e213d03a05727d7cbcac15578967
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-24 19:38:18 +0200

    Add while loop.

--- TaskSet04/Task0408.java
+++ TaskSet04/Task0408.java
@@ -6,7 +6,7 @@ package TaskSet04;
  * TASK:
  * Try to implement a while loop that prints the love message 
  * "love, love everywhere" 10 times by updating the index 
- * \inside the while block, by directly updating with pre-
+ * inside the while block, by directly updating with pre-
  * increment in the condition statement.  
  * 
  * Does it matter if you use pre-increment (++i) or post-
@@ -20,11 +20,17 @@ package TaskSet04;
 public class Task0408 {
 
     // Constants
+    private static String MESSAGE = "love, love everywhere";
 
     public static void main(String[] args) {
 
         // Variables
-        
+        int counter = 1;
+
+        while (counter <= 10) {
+            System.out.printf("%d. %s%n", counter++, MESSAGE);
+        }
+
     }
     
 }
