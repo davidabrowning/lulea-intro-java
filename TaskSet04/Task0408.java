c01a9d541022436c36b2108436f144d00f2e7f50
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-24 19:32:24 +0200

    Create .java file, add scaffolding, add task description.

---
+++ TaskSet04/Task0408.java
@@ -0,0 +1,30 @@
+package TaskSet04;
+
+// Imports
+
+/**
+ * TASK:
+ * Try to implement a while loop that prints the love message 
+ * "love, love everywhere" 10 times by updating the index 
+ * \inside the while block, by directly updating with pre-
+ * increment in the condition statement.  
+ * 
+ * Does it matter if you use pre-increment (++i) or post-
+ * increment (i++) in the condition? Try changing how you 
+ * make the increments and test rerun your program with the 
+ * new implementation.
+ * 
+ * @author David Browning (davbro-4)
+ */
+
+public class Task0408 {
+
+    // Constants
+
+    public static void main(String[] args) {
+
+        // Variables
+        
+    }
+    
+}
