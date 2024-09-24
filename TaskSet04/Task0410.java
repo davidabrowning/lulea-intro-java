3d20e8522d3b5fa1affe8bbb74803e1276bc09b7
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-24 20:06:58 +0200

    Add program scaffolding.

--- TaskSet04/Task0410.java
+++ TaskSet04/Task0410.java
@@ -1,5 +1,23 @@
 package TaskSet04;
 
+// Imports
+
+/**
+ * TASK:
+ * 
+ * DESCRIPTION:
+ * 
+ * @author David Browning (davbro-4)
+ */
 public class Task0410 {
+
+    // Constants
+
+    public static void main(String[] args) {
+
+        // Variables
+
+
+    }
     
 }
