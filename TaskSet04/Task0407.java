1a0c3b6062b5d9bfe7bb1d853bc4eaa33ffab217
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-24 17:45:42 +0200

    Create file, add description, add scaffolding.

---
+++ TaskSet04/Task0407.java
@@ -0,0 +1,18 @@
+package TaskSet04;
+
+/**
+ * DESCRIPTION:
+ * For a given loop, you always want to run the loop at least once before 
+ * the user's input of an integer causes the loop to end. You can enter 
+ * the following example and see that the loop is run at least once 
+ * before the condition is checked.
+ * 
+ * @author David Browning (davbro-4)
+ */
+public class Task0407 {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
