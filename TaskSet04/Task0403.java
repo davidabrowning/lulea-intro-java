d74363c96484023d85eb91f2cb8483e826b281a2
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-24 12:27:23 +0200

    Add input prompt, input validation, and output print.

--- TaskSet04/Task0403.java
+++ TaskSet04/Task0403.java
@@ -26,10 +26,17 @@ import java.util.Scanner;
 public class Task0403 {
 
     // Declare constants
-    private static char MENU_QUIT_KEY = 'Q';
+    private static String MENU_QUIT_KEY = "Q";
+    private static String USER_INPUT_PROMPT = "Please enter your weekly allowance in kronor (q to quit): ";
+    private static String INVALID_INPUT_TEXT = "Invalid weekly allowance.";
+    private static String MENU_EXIT_TEXT = "Exiting program.";
     private static int SINGLE_SEK = 10;
     private static int DOUBLE_SEK = 15;
     private static int TRIPLE_SEK = 20;
+    private static String ZERO_STATEMENT = "You cannot afford any ice cream this week.";
+    private static String SINGLE_STATEMENT = "You can buy a single scoop!";
+    private static String DOUBLE_STATEMENT = "You can buy a double scoop!";
+    private static String TRIPLE_STATEMENT = "You can buy a triple scoop!";
 
     public static void main(String[] args) {
 
@@ -40,8 +47,32 @@ public class Task0403 {
         // Menu loop
         while (true) {
 
+            // 1. Ask user for weekly allowance amount
+            System.out.printf(USER_INPUT_PROMPT);
 
-            
+            // 2. Validate user input
+            if (userInput.next().toUpperCase().equals(MENU_QUIT_KEY)) {
+                System.out.printf("%s%n", MENU_EXIT_TEXT);
+                userInput.close();
+                return;
+            }
+            if (!userInput.hasNextInt()) {
+                System.out.printf("%s%n", INVALID_INPUT_TEXT);
+                continue;
+            }
+            weeklyAllowance = userInput.nextInt();
+
+            // 3. Print which option the user can buy
+            if (weeklyAllowance < SINGLE_SEK) {
+                System.out.printf("%s%n", ZERO_STATEMENT);
+            } else if (weeklyAllowance < DOUBLE_SEK) {
+                System.out.printf("%s%n", SINGLE_STATEMENT);
+            } else if (weeklyAllowance < TRIPLE_SEK) {
+                System.out.printf("%s%n", DOUBLE_STATEMENT);
+            } else {
+                System.out.printf("%s%n", TRIPLE_STATEMENT);
+            }
+
         }
 
     }
