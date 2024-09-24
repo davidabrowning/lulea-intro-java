b17b4a2dc08993a80318f7759d0a88609569075c
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-24 14:32:24 +0200

    Add task description.

--- TaskSet04/Task0405.java
+++ TaskSet04/Task0405.java
@@ -4,6 +4,17 @@ package TaskSet04;
 
 /**
  * TASK:
+ * 1.   Animals are generally unisexual, i.e., either female or male. 
+ *      Create a logical expression with appropriate relationship 
+ *      operators that determine whether a particular individual is 
+ *      male or female. Use two different variables to represent the 
+ *      male. Thus a Boolean variable is true about males and a 
+ *      Boolean variable is true about females. Show how you, with 
+ *      exclusive OR (^), can implement a solution where only one 
+ *      gender at a time gives true.
+ * 2.   Worms are bisexual hermaphrodites, i.e., both male/female. 
+ *      Complement your solution so that the combination of male and 
+ *      female simultaneously shows the result of hermaphrodite.
  * 
  * PROGRAM DESCRIPTION:
  * 
