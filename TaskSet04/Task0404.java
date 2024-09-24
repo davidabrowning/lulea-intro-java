3faa7d9a70c01f2d1aed8dabe9f3aadfcf9df94d
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-24 13:06:33 +0200

    Add task description.

--- TaskSet04/Task0404.java
+++ TaskSet04/Task0404.java
@@ -4,6 +4,18 @@ package TaskSet04;
 
 /**
  * TASK:
+ * For a particular program, you want to use an entered time value to 
+ * tell whether it is lunchtime or not. Lunchtime is defined as the 
+ * interval between the times 1200 -1300.
+ *      -   Think about how the combination of logical operators and 
+ *          comparison operators can describe a compound logical 
+ *          expression to decide about whether it is lunch or not for 
+ *          the entered time. In this case, you can represent the 
+ *          time as an integer, given that you do not use a colon. 
+ *          1200 is entered and not 12:00
+ *      -   Show an alternative solution where you replace your 
+ *          logical operator with a nested if statement (one if 
+ *          statement inside another if statement)
  * 
  * PROGRAM DESCRIPTION:
  * 
