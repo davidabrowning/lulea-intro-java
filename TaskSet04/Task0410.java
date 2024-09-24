1dcc2e2b772aab04318fe0654b8b76f867cc4f38
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-24 20:10:49 +0200

    Add task description and program description.

--- TaskSet04/Task0410.java
+++ TaskSet04/Task0410.java
@@ -4,8 +4,19 @@ package TaskSet04;
 
 /**
  * TASK:
+ * 1.   Make a loop ( for ) that prints the interval of integer digits 0..9
+ * 2.   Construct a loop ( for ) that prints all lowercase letters of the 
+ *      English alphabet. Use the decimal code (ASCII code) found in the 
+ *      Unicode table. Use type cast from integer to char to make a simple 
+ *      conversion between the index of the decimal code and the resulting 
+ *      character. (*)
+ * 3.   Modify the above solution so vowels and consonants can be 
+ *      identified during iteration. (*)
  * 
- * DESCRIPTION:
+ * PROGRAM DESCRIPTION:
+ * 1. Loop to print intervals 0..9
+ * 2. Loop to print all lowercase letters
+ * 3. Loop to print all lowercase letters, indicating vowels and consonants
  * 
  * @author David Browning (davbro-4)
  */
@@ -16,6 +27,9 @@ public class Task0410 {
     public static void main(String[] args) {
 
         // Variables
+
+        // 1. Loop to print intervals 0..9
+        
 
 
     }
