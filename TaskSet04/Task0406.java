a4e5fc872caf80d950da961813cfffd2a43d4734
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-24 15:05:45 +0200

    Create .java file.

---
+++ TaskSet04/Task0406.java
@@ -0,0 +1,16 @@
+package TaskSet04;
+
+// Imports
+
+/**
+ * TASK: 
+ * Construct a loop executed until the user enters a number that is not greater than zero.
+ * Modify the loop so the number must be less than zero to achieve termination.
+ * 
+ * PROGRAM DESCRIPTION:
+ * 
+ */
+
+public class Task0406 {
+    
+}
