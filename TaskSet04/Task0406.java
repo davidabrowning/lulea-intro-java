96c505861bb75b9de16e2032f4626ae8774713d3
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-24 15:08:35 +0200

    Add program description.

--- TaskSet04/Task0406.java
+++ TaskSet04/Task0406.java
@@ -8,9 +8,18 @@ package TaskSet04;
  * Modify the loop so the number must be less than zero to achieve termination.
  * 
  * PROGRAM DESCRIPTION:
+ * 1. Print instructions
+ * 2. Collect input
+ * 3. Validate input
+ * 4. Check criterion for exiting loop
  * 
+ * @author David Browning (davbro-4)
  */
 
 public class Task0406 {
+
+    public static void main(String[] args) {
+
+    }
     
 }
