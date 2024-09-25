24188c1dfc38d41fabc90eff14eea00e8f843b32
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-25 15:50:14 +0200

    Addressed minor code style warnings from Codegrade.

--- Assignment01/Assignment01Dice/Main.java
+++ Assignment01/Assignment01Dice/Main.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 
 /**
  * PROGRAM SUMMARY:
- *  The program is a game where the player will roll 3 dice to get a total 
+ *  The program is a game where the player will roll 3 dice to get a total
  *  sum of 12 in order to win.
- * 
+ *
  * PROGRAM DESCRIPTION:
  *  1. Print game instructions
  *  2. Loop until user enters q
  *  3. Ask for user to select a die to roll
  *  4. Validate user's die selection
- *  5. Process die roll 
+ *  5. Process die roll
  *      5a. Log this roll
  *      5b. Check if round over
  *      5c. Check for win/loss
@@ -33,20 +33,21 @@ import java.util.Scanner;
 public class Main {
 
     // Constants
-    private static final String GAME_START = "Welcome to dice game 12. You must roll 1-3"
-        + " dice and try to get the sum of 12 ...\n";
-    private static final String CHOOSE_DIE = "Enter which dice you want to roll [1,2,3]"
-        + " (exit with q):";
+    private static final String GAME_START = "Welcome to dice game 12."
+        + " You must roll 1-3 dice and try to get the sum of 12 ...\n";
+    private static final String CHOOSE_DIE = "Enter which dice you want"
+        + " to roll [1,2,3] (exit with q):";
     private static final String EXIT_STRING = "q";
     private static final String ROUND_WON = "You won!!";
     private static final String ROUND_LOST = "You lost!!";
-    private static final String ROUND_TIE = "You neither won nor lost the game.";
+    private static final String ROUND_TIE = "You neither won nor lost"
+        + " the game.";
     private static final String NEXT_ROUND = "Next round!";
     private static final String GAME_OVER = "Game Over!";
-    private static final String ALREADY_SELECTED_DICE = "Sorry, you have already rolled"
-        + " that dice. Try again";
-    private static final String INVALID_ENTRY = "Sorry, that is an invalid entry. Try"
-        + " again. Valid entries are 1, 2, 3, and q\n";
+    private static final String ALREADY_SELECTED_DICE = "Sorry, you have"
+        + " already rolled that dice. Try again";
+    private static final String INVALID_ENTRY = "Sorry, that is an invalid"
+        + " entry. Try again. Valid entries are 1, 2, 3, and q\n";
     private static final String AMOUNT_WIN_STRING = "#win:";
     private static final String AMOUNT_LOST_STRING = "#loss:";
     private static final String SUM_STRING = "sum:";
@@ -54,28 +55,28 @@ public class Main {
     private static final int MIN_DIE_VALUE = 1;
     private static final int DICE_SUM_TARGET_VALUE = 12;
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
         // Variables
         String userMenuSelection = "";  // The user's non-integer menu selection
         int userDieSelection = 0;       // The user's selection of die to roll
-        int userDieRollResult = 0;      // The result of the most current die roll
+        int userDieRollResult = 0;      // The result of the most current roll
 
         int die1Value = 0;              // The value of the first die
-        boolean isDie1Rolled = false;   // Whether the first die is rolled or not
+        boolean isDie1Rolled = false;   // Whether the first die is rolled
 
         int die2Value = 0;              // The value of the second die
-        boolean isDie2Rolled = false;   // Whether the second die is rolled or not
+        boolean isDie2Rolled = false;   // Whether the second die is rolled
 
         int die3Value = 0;              // The value of the third die
-        boolean isDie3Rolled = false;   // Whether the third die is rolled or not
+        boolean isDie3Rolled = false;   // Whether the third die is rolled
 
         int sum = 0;                    // The sum of the die values
         int winCounter = 0;             // The number of wins
         int lossCounter = 0;            // The number of losses
 
         boolean roundIsOver = false;    // Whether this round is over or not
-        boolean isPlaying = true;       // Whether the game is being played or not
+        boolean isPlaying = true;       // Whether the game is being played
 
         Scanner userInput = new Scanner(System.in);
         Random rand = new Random();
@@ -152,13 +153,13 @@ public class Main {
                 if (sum > DICE_SUM_TARGET_VALUE) {
                     roundIsOver = true;
                     lossCounter++;
-                }                
+                }
             }
 
             // 6. Print die status and win/loss status
             System.out.printf("%d %d %d %s %d %s %d %s %d%n",
-                die1Value, die2Value, die3Value, SUM_STRING, sum, 
-                AMOUNT_WIN_STRING, winCounter, AMOUNT_LOST_STRING, 
+                die1Value, die2Value, die3Value, SUM_STRING, sum,
+                AMOUNT_WIN_STRING, winCounter, AMOUNT_LOST_STRING,
                 lossCounter);
 
             // 7. If round over
@@ -190,7 +191,7 @@ public class Main {
 
         // 9. If game over:
         // 9a. Print win/loss status
-        System.out.printf("%s %d %s %d%n", AMOUNT_WIN_STRING, 
+        System.out.printf("%s %d %s %d%n", AMOUNT_WIN_STRING,
             winCounter, AMOUNT_LOST_STRING, lossCounter);
 
         // 9b. Print Game Over
