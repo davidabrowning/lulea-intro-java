16baa4c2391396e5f611e2cc549d7dbd4d08a524
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-25 11:10:46 +0200

    Fix misspellings of dice to die.

--- Assignments/Assignment01Dice/Main.java
+++ Assignments/Assignment01Dice/Main.java
@@ -32,34 +32,38 @@ class Main {
    * You can rename them if you want, and you can directly use them in your code.
    * Just make sure these exact messages are used.
    */
-    static final String GAME_START = "Welcome to dice game 12. You must roll 1-3 dice and try to get the sum of 12 ...\n";
-    static final String CHOOSE_DICE = "Enter which dice you want to roll [1,2,3] (exit with q):";
+    static final String GAME_START = "Welcome to dice game 12. You must roll 1-3"
+        + " dice and try to get the sum of 12 ...\n";
+    static final String CHOOSE_DIE = "Enter which die you want to roll [1,2,3]"
+        + " (exit with q):";
     static final String ROUND_WON = "You won!!";
     static final String ROUND_LOST = "You lost!!";
     static final String ROUND_TIE = "You neither won nor lost the round.";
     static final String NEXT_ROUND = "Next round!";
     static final String GAME_OVER = "Game Over!";
-    static final String ALREADY_SELECTED_DICE = "Sorry, you have already rolled that dice. Try again";
-    static final String INVALID_ENTRY = "Sorry, that is an invalid entry. Try again. Valid entries are 1, 2, 3, and q\n";
+    static final String ALREADY_SELECTED_DICE = "Sorry, you have already rolled"
+        + "that die. Try again";
+    static final String INVALID_ENTRY = "Sorry, that is an invalid entry. Try"
+        + " again. Valid entries are 1, 2, 3, and q\n";
     static final String AMOUNT_WIN_STRING = "#win: ";
     static final String AMOUNT_LOST_STRING = " #loss: ";
     static final String SUM_STRING = " sum: ";
-    static final int MAX_DICE_VALUE = 6;
-    static final int MIN_DICE_VALUE = 1;
+    static final int MAX_DIE_VALUE = 6;
+    static final int MIN_DIE_VALUE = 1;
     static final int DICE_SUM_TARGET_VALUE = 12;
 
     public static void main(final String[] args) {
 
-        int dice1Value = 0; // The value of the first dice
-        boolean isDice1Rolled = false; // Whether the first dice is rolled or not
+        int die1Value = 0; // The value of the first die
+        boolean isDie1Rolled = false; // Whether the first die is rolled or not
 
-        int dice2Value = 0; // The value of the second dice
-        boolean isDice2Rolled = false; // Whether the second dice is rolled or not
+        int die2Value = 0; // The value of the second die
+        boolean isDie2Rolled = false; // Whether the second die is rolled or not
 
-        int dice3Value = 0; // The value of the third dice
-        boolean isDice3Rolled = false; // Whether the third dice is rolled or not
+        int die3Value = 0; // The value of the third die
+        boolean isDie3Rolled = false; // Whether the third die is rolled or not
 
-        int sum = 0; // The sum of the dice values
+        int sum = 0; // The sum of the die values
         int winCounter = 0; // The number of wins
         int lossCounter = 0; // The number of losses
 
@@ -71,7 +75,7 @@ class Main {
         System.out.println(GAME_START);
 
         while (isPlaying) {
-            System.out.println(CHOOSE_DICE);
+            System.out.println(CHOOSE_DIE);
             
             isPlaying = false;
         }
