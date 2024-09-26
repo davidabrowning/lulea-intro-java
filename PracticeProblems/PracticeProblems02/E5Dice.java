3340eee362aa2a27167474b2aa90b86c54d8f0aa
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-26 17:09:47 +0200

    Add task description, author tag, main method.

--- PracticeProblems/PracticeProblems02/E5Dice.java
+++ PracticeProblems/PracticeProblems02/E5Dice.java
@@ -1,6 +1,24 @@
 package PracticeProblems.PracticeProblems02;
 
+/**
+ * TASK:
+ * Write a program where the user is first asked to enter the number of dice
+ * rolls desired. The program must then print how many times a dice number
+ * appeared, that is, show the number of 1s, 2s, etc. Instead of a count,
+ * one should show the #s of characters.
+ * 
+ * 
+ * @author David Browning (davbro-4)
+ */
 public class E5Dice {
+
+    // Constants
+
+    public static void main(final String[] args) {
+
+        // Variables
+
+    }
 
     
 }
