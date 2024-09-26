94ce2c781a30380c183a67767349c0dfe378c429
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-26 16:57:22 +0200

    Removing Javadoc comments from practice problem I will not do.

--- PracticeProblems/PracticeProblems02/A1Temperature.java
+++ PracticeProblems/PracticeProblems02/A1Temperature.java
@@ -1,23 +1,5 @@
 package PracticeProblems.PracticeProblems02;
 
-/**
- * TASK:
- * The temperature task structured on Practice Problem 1 (Available only on 
- * Canvas) will now be partially implemented in this task. The code in 
- * Main.java represents the basic structure. Fill in the code under the 
- * headings "declaration of variables", "start," and "print" while 
- * "processing" is saved to another problem titled "Logic - Temperature" 
- * later in the same module.
- * 
- * Just declare the variables that will be needed. Then, ask the user to 
- * enter the needed data. In this case, just the number of days, then print 
- * the variables with the appropriate format and details.
- * 
- * PROGRAM DESCRIPTION:
- * 
- * 
- * @author David Browning (davbro-4)
- */
 public class A1Temperature {
     
 }
