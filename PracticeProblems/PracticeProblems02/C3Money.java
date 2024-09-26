cb48bb71d0bca35f32cb2458a2c5d9bd9e78f4b2
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-26 16:32:42 +0200

    Add section headers.

--- PracticeProblems/PracticeProblems02/C3Money.java
+++ PracticeProblems/PracticeProblems02/C3Money.java
@@ -15,4 +15,20 @@ package PracticeProblems.PracticeProblems02;
  */
 public class C3Money {
 
+    // Constants
+
+    public static void main(final String[] args) {
+
+        // Variables
+
+        // 1. Ask user for amount of money to exchange
+
+        // 2. Separate into kronor amounts
+        
+        // 3. Display results
+        
+        // 4. Close and exit
+        
+    }
+
 }
