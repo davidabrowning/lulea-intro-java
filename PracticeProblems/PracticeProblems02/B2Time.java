b8f7ccf27818595219cdffb6efe1296a7e06ca7e
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-26 14:28:38 +0200

    Validate user input.

--- PracticeProblems/PracticeProblems02/B2Time.java
+++ PracticeProblems/PracticeProblems02/B2Time.java
@@ -24,7 +24,7 @@ public class B2Time {
     // Constants
     private static String TIME_INPUT_INSTRUCTIONS = "Please enter a time amount (HH:MM:SS): ";
     private static String TIME_INPUT_DELIMITER = ":|\\s+";
-    private static String INVALID_TIME_MESSAGE = "Invalid time entered.";
+    private static String INVALID_TIME_MESSAGE = "Invalid time entered. Exiting program.";
     private static int SECONDS_PER_MINUTE = 60;
     private static int SECONDS_PER_HOUR = 3600;
     private static String TIME_OUTPUT_MESSAGE_START = "The time you entered is: ";
@@ -43,6 +43,39 @@ public class B2Time {
 
         // 1. Ask for user input (HH:MM:SS)
         System.out.printf("%s", TIME_INPUT_INSTRUCTIONS);
+
+        // 2. Validate user input
+
+        // 2a. Set delimiter
+        userInput.useDelimiter(TIME_INPUT_DELIMITER);
+
+        // 2b. Check for missing hour/minute/second values
+        if (userInput.hasNextInt()) {
+            userHours = userInput.nextInt();
+        } else {
+            System.out.printf("%s%n", INVALID_TIME_MESSAGE);
+            System.exit(0);
+        }
+        if (userInput.hasNextInt()) {
+            userMinutes = userInput.nextInt();
+        } else {
+            System.out.printf("%s%n", INVALID_TIME_MESSAGE);
+            System.exit(0);
+        }
+        if (userInput.hasNextInt()) {
+            userSeconds = userInput.nextInt();
+        } else {
+            System.out.printf("%s%n", INVALID_TIME_MESSAGE);
+            System.exit(0);
+        }
+
+        // 2c. Check for illegal hour/minute/second values
+        if (userHours < 0 || userMinutes < 0 || userMinutes > 59 || userSeconds < 0 || userSeconds > 59) {
+            System.out.printf("%s%n", INVALID_TIME_MESSAGE);
+            System.exit(0);            
+        }
+
+        
 
     }
     
