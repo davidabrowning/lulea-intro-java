fb78b6f8ccb946e8bcd6680c560b8298e9ca6192
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-26 16:30:26 +0200

    Add task description.

--- PracticeProblems/PracticeProblems02/C3Money.java
+++ PracticeProblems/PracticeProblems02/C3Money.java
@@ -1,5 +1,15 @@
 package PracticeProblems.PracticeProblems02;
 
+/**
+ * TASK:
+ * Write a Java program that divides the user's amount into 20-kronor, 
+ * 10-kronor, 5-kronor, and 1-kronor
+ * 
+ * PROGRAM DESCRIPTION:
+ * 
+ * 
+ * @author David Browning (davbro-4)
+ */
 public class C3Money {
-    
+
 }
