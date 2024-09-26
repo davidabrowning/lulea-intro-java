8a066eb6f789cfc393a0a809630027c4f0635457
David <11443294+davidabrowning@users.noreply.github.com>
2024-09-26 14:15:44 +0200

    Add program description, main method.

---
+++ PracticeProblems/PracticeProblems02/B2Time.java
@@ -0,0 +1,27 @@
+package PracticeProblems.PracticeProblems02;
+
+/**
+ * TASK:
+ * Write a Java program that asks for the number of hours, minutes, and 
+ * seconds. Use the Scanner class with the colon sign ':' as a delimiter 
+ * (see below). The program will then calculate and print how many 
+ * seconds it was in total.
+ * 
+ * PROGRAM DESCRIPTION:
+ * 1. Ask for user input (HH:MM:SS)
+ * 2. Validate user input
+ * 3. Calculate number of seconds
+ * 4. Display number of seconds
+ * 
+ * 
+ * @author David Browning (davbro-4)
+ */
+public class B2Time {
+
+    // Constants
+
+    public static void main(final String[] args) {
+        
+    }
+    
+}
