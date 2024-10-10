e53540bafed327305f552997a09b05c1e7277d50
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-10 10:22:53 +0200

    Add comment headers for includes, Javadoc, constants, and variables.

--- MarkedAssignments/Assignment02RandomNumbers/Main.java
+++ MarkedAssignments/Assignment02RandomNumbers/Main.java
@@ -1,8 +1,19 @@
 package MarkedAssignments.Assignment02RandomNumbers;
 
+// Includes
+
+/**
+ * PROGRAM DESCRIPTION
+ * 
+ */
 class Main {
 
+    // Declare constants
+
     public static void main(final String[] args) {
+
+        // Declare variables
+
         System.out.println("Hello world!");
     }
 }
