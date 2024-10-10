ffd5d9a90dde7b456f7838cc5a7d9ab6bb6f1e96
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-10 10:24:43 +0200

    Add constants from Codegrade template.

--- MarkedAssignments/Assignment02RandomNumbers/Main.java
+++ MarkedAssignments/Assignment02RandomNumbers/Main.java
@@ -11,6 +11,12 @@ package MarkedAssignments.Assignment02RandomNumbers;
 class Main {
 
     // Declare constants
+    static final String USER_INPUT_PROMPT = "How many random numbers in the range 0 - 999 are desired?";
+    static final String RANDOM_NUMBERS_LIST_MESSAGE = "Here are the random numbers:";
+    static final String RANDOM_NUMBERS_SORTED_MESSAGE = "Here are the random numbers arranged:";
+    static final String NO_ODD_NUMBERS_MESSAGE = "No Odd Numbers";
+    static final String NO_EVEN_NUMBERS_MESSAGE = "No Even Numbers";
+    static final String INVALID_INPUT_MESSAGE = "Invalid Input";    
 
     public static void main(final String[] args) {
 
