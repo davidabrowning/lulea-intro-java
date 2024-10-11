47b5e7d8a62dbf7ee1adfeb4759192b82dc2116e
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-11 13:43:18 +0200

    Create practice problem files and add task description comments.

---
+++ PracticeProblems/PracticeProblems03/PP0301Dice.java
@@ -0,0 +1,7 @@
+package PracticeProblems.PracticeProblems03;
+
+/**
+ * === Task description ===
+ * Modify the dice program from the previous module on logic so that the frequency calculation is done using an array.
+ * 
+ */
---
+++ PracticeProblems/PracticeProblems03/PP0303Sorting.java
@@ -0,0 +1,10 @@
+package PracticeProblems.PracticeProblems03;
+
+/**
+ * === Task description ===
+ * Write a program that randomizes 30 integers from 1 to 100 into an array a, sorts them, and prints them.
+ */
+
+public class PP0303Sorting {
+    
+}
