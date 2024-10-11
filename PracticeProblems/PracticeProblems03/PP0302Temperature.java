97aa982d97493d3a60d8b609f5118a3a3945c757
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-11 14:06:13 +0200

    Address long lines.

--- PracticeProblems/PracticeProblems03/PP0302Temperature.java
+++ PracticeProblems/PracticeProblems03/PP0302Temperature.java
@@ -2,6 +2,9 @@ package PracticeProblems.PracticeProblems03;
 
 /**
  * === Task description ===
- * Modify the temperature data from the previous module on logic to store all temperature values in an array. The program must print out which day the highest and lowest temperature was measured, what temperatures it was, and the number of days when it exceeded the average temperature.
+ * Modify the temperature data from the previous module on logic to store all
+ * temperature values in an array. The program must print out which day the
+ * highest and lowest temperature was measured, what temperatures it was, and
+ * the number of days when it exceeded the average temperature.
  * 
  */
