5a1dc35d20e327093d9b78d07c6e4ada73ae1261
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-11 14:21:58 +0200

    Populate array, print unsorted array.

--- PracticeProblems/PracticeProblems03/PP0303Sorting.java
+++ PracticeProblems/PracticeProblems03/PP0303Sorting.java
@@ -18,6 +18,8 @@ public class PP0303Sorting {
 
     // 1. Declare constants
     static int NUMBERS_TO_GENERATE = 30;
+    static int RANGE_MIN = 1;
+    static int RANGE_MAX = 100;
 
     public static void main(final String[] args) {
         // 2. Declare variables
@@ -26,8 +28,14 @@ public class PP0303Sorting {
         int temp = 0;       // Assists with swapping array elements
 
         // 3. Populate random number array
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (Math.random() * 99) + 1;
+        }
 
         // 4. Print unsorted random number array
+        for(int i : numbers) {
+            System.out.print(i + " ");
+        }
 
         // 5. Sort array with selection sort
 
