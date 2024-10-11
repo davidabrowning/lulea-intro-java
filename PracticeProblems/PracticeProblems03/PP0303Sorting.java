d98e369277f340664223e1e4f2477dc7b34b487f
David <11443294+davidabrowning@users.noreply.github.com>
2024-10-11 14:14:03 +0200

    Add section header comments. Add main method.

--- PracticeProblems/PracticeProblems03/PP0303Sorting.java
+++ PracticeProblems/PracticeProblems03/PP0303Sorting.java
@@ -3,8 +3,31 @@ package PracticeProblems.PracticeProblems03;
 /**
  * === Task description ===
  * Write a program that randomizes 30 integers from 1 to 100 into an array a, sorts them, and prints them.
+ * 
+ * === Program description ===
+ * 1. Declare constants
+ * 2. Declare variables
+ * 3. Populate random number array
+ * 4. Print unsorted random number array
+ * 5. Sort array with selection sort
+ * 6. Print sorted array
+ * 
+ * @author David Browning (davbro-4)
  */
-
 public class PP0303Sorting {
-    
+
+    // 1. Declare constants
+
+    public static void main(final String[] args) {
+        // 2. Declare variables
+        
+        // 3. Populate random number array
+
+        // 4. Print unsorted random number array
+
+        // 5. Sort array with selection sort
+
+        // 6. Print sorted array
+
+    }
 }
